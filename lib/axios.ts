dd59ebcc39a51336871ee21eaf7d8ad2c56b1889
Darius Daniel <dariusdaniel771@gmail.com>
2026-06-13 14:47:31 +0100

    fix: remove custom header from axios configuration

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.BASE_URL + "/api/",
   timeout: 5000,
-  headers: { "X-Custom-Header": "foobar" },
+  // headers: { "X-Custom-Header": "foobar" },
 });
 
 export default axiosInstance;
