de2efdf08433982896ba55d038b21eb29c9a36f0
Darius Daniel <dariusdaniel771@gmail.com>
2026-06-13 14:54:57 +0100

    refactor: remove process.env prefix from axios base URL to use relative path

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.BASE_URL + "/api/",
+  baseURL: "/api/",
   timeout: 5000,
   // headers: { "X-Custom-Header": "foobar" },
 });
